fcfd227813d0e49a092af08813db60338d3c4773
stamovm <m.stamov@yahoo.com>
2021-11-10 21:33:48 -0800

    updated the project to use json server

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -4,40 +4,89 @@ import AddItem from './AddItem'
 import SearchItem from './SearchItem'
 import Content from './Content'
 import Footer from './Footer'
+import apiRequest from './apiRequest'
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist')) || []
-  )
+  const API_URL = 'http://localhost:3500/items'
+
+  const [items, setItems] = useState([])
+  //JSON.parse(localStorage.getItem('shoppinglist')) || []
   const [newItem, setNewItem] = useState([''])
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items))
-  }, [items])
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        console.log('response: ', response)
+        if (!response.ok) throw Error('Did not receive data')
+        const listItems = await response.json()
+
+        setItems(listItems)
+        setFetchError(null)
+      } catch (err) {
+        setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    setTimeout(() => {
+      ;(async () => await fetchItems())()
+    }, 2000)
+  }, [])
+  //localStorage.setItem('shoppinglist', JSON.stringify(items))
 
   const setAndsaveItems = (newItems) => {
     setItems(newItems)
     localStorage.setItem('shoppinglist', JSON.stringify(newItems))
   }
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem = { id, checked: false, item }
     const listItems = [...items, myNewItem]
     setAndsaveItems(listItems)
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(myNewItem),
+    }
+    const result = await apiRequest(API_URL, postOptions)
+    if (result) setFetchError(result)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     )
     setAndsaveItems(listItems)
+
+    const myItem = listItems.filter((item) => item.id === id)
+    const updateOptons = {
+      method: 'patch',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptons)
+    if (result) setFetchError(result)
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setAndsaveItems(listItems)
+
+    const deleteOptions = { method: 'DELETE' }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, deleteOptions)
+    if (result) setFetchError(result)
   }
 
   const handleSubmit = (e) => {
@@ -56,15 +105,18 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        className="main"
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
-        )}
-        setItems={setItems}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        <Content
+          className="main"
+          items={items.filter((item) =>
+            item.item.toLowerCase().includes(search.toLowerCase())
+          )}
+          setItems={setItems}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      </main>
       <Footer className="footer" length={items.length} />
     </div>
   )
--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -2,7 +2,7 @@ import ItemList from './ItemList'
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
-    <main>
+    <>
       {items.length ? (
         <ItemList
           items={items}
@@ -14,7 +14,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
           The list is empty.
         </p>
       )}
-    </main>
+    </>
   )
 }
 
